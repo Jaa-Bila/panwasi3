ceeb61c9c59bb899d5a501a84f8da113c18edc2b
jaa_bila <jaabiladunilakhsan@gmail.com>
2023-06-15 23:42:29 +0700

    first commit

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -14,9 +14,8 @@
                     </svg>
                 </button>
                 <a href="https://flowbite.com" class="flex ml-2 md:mr-24">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="FlowBite Logo" />
                     <span
-                        class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Flowbite</span>
+                        class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Panwasi</span>
                 </a>
             </div>
             <div class="flex items-center">
